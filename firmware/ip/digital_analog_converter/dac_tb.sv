36c492f23157d7279024132bae235fcdc2e43ff5
Jessica Liu <jesliu@hmc.edu>
2025-09-26 14:34:14 -0700

    dac_tb synth

--- firmware/ip/digital_analog_converter/dac_tb.sv
+++ firmware/ip/digital_analog_converter/dac_tb.sv
@@ -5,21 +5,19 @@
 module dac_tb();
 
     logic              clk;
-    logic [bits-1:0]   digital_in;
+    logic [bits-1:0]   s_axis_data;
     real               vref;
     real               aout;
     logic [31:0]       errors;
 
+    
     dac dut(
         .clk(clk),
-        .digital_in(digital_in),
-        .vref(vref),
+        .s_axis_tdata(s_axis_data),
         .aout(aout)
     );
 
     parameter int bits = dut.bits;    // DAC resolution in bits
-    parameter time td = dut.td;       // Processing delay of DAC
-    real aout_reg;
     real expected_out;
 
     // Generate clock: 10 time units period (5 high, 5 low)
@@ -31,22 +29,22 @@ module dac_tb();
     // Start of test
     initial begin
         // Initialize inputs
-        digital_in = 32'd0;
+        s_axis_data = 16'd0;
         errors = 0;
         vref = 5.0;
 
         #10;
-        assert(dut.digital_in == 4'b0000) else begin
+        assert(s_axis_data == 4'b0000) else begin
             $error("Input digital signal is not 0");
             errors++;
         end
 
-        digital_in <= '1;// biggest input
+        s_axis_data <= '1;// biggest input
         
          #50;         
          expected_out = vref * (real'(2**bits - 1)) / (real'(2**bits));
          
-         assert($abs(aout - expected_out) < 1e-9) else begin
+         assert(((aout - expected_out) < 1e-9) ? -(aout - expected_out) : (aout - expected_out)) else begin
             $error("aout is not its expected value with a max input");
             errors++;
         end
